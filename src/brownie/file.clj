e694747e8eecc8f8746386cd3fe22f2a1a9c2cdf
Ivan Kryvoruchko <gildraug@gmail.com>
2013-07-03 00:14:01 +0300

    First draft of collecting files up to specified size.

--- src/brownie/file.clj
+++ src/brownie/file.clj
@@ -18,3 +18,26 @@
   directory subtree that starts at given root."
   [root & exts]
   (apply find-by-regexp root (map #(re-pattern (str ".+" "\\." %)) exts)))
+
+;;; TODO should be random (i. e., collect different files on each run)
+(defn collect-files
+  [max-size filepaths]
+  (let [files (shuffle (map fs/file filepaths))
+        file-size-tuples (shuffle (map (fn [f] [f (.length f)]) files))
+        tuples->map (fn [m [f s]]
+                      (let [files (conj (get m s #{}) f)] (assoc m s files)))
+        size-files-map (reduce tuples->map (sorted-map) file-size-tuples)
+        collect (fn [{:keys [total-size] :as m} [size files]]
+                  (if (> size 0)
+                    (let [num-of-files-to-take
+                          (min (count files)
+                               (Math/floor (/ (- max-size total-size) size)))
+
+                          files-size
+                          (* size num-of-files-to-take)]
+                      (-> m
+                          (update-in [:files] into
+                                     (take num-of-files-to-take files))
+                          (update-in [:total-size] + files-size)))
+                    m))]
+    (reduce collect {:files #{} :total-size 0} size-files-map)))
