5c99e56dd161c4c67e7f279d675d96cbbb42913b
Ivan Kryvoruchko <gildraug@gmail.com>
2013-07-04 23:23:13 +0300

    'Good enough' function to gather random files.

--- src/brownie/tasks.clj
+++ src/brownie/tasks.clj
@@ -1,27 +1,17 @@
 (ns brownie.tasks
-  (:require [brownie.tools.file :as f]
-            [clojure.math.combinatorics :as combo]))
+  (:require [brownie.tools.file :as f]))
 
-;;; TODO better algo (not to so slow - generating all subsets is very
-;;;      'expensive')
 (defn gather-files-for-size
-  "Makes a list of random files from given root directory (and its
+  "Makes a set of random files from given root directory (and its
   subdirectories) with given extensions. The total size of gathered files is
-  smaller or equal to the given maximum size (specified in megabytes) but at
-  least as big as given percent of maximum size (float in range (0.0, 1.0])."
-  [min-size-percent max-size root & extensions]
-  {:pre [(number? min-size-percent) (number? max-size)
-         (or (string? root) (instance? java.io.File root))
-         (every? string? extensions)
-         (> min-size-percent 0.0) (<= min-size-percent 1.0)]}
-  (let [max-size (* max-size 1024 1024)
-        possible-combinations
-        (->> (apply f/find-by-extension root extensions)
-             f/paths->files
-             combo/subsets
-             (map (fn [x] [x (f/total-size x)]))
-             (filter (fn [[files total-size]]
-                       (and (>= max-size total-size)
-                            (> (/ total-size max-size) min-size-percent)))))]
-    (when-not (empty? possible-combinations)
-      (first (rand-nth (shuffle possible-combinations))))))
+  smaller or equal to the given maximum size (specified in megabytes)."
+  [max-size root & extensions]
+  {:pre [(number? max-size) (or (string? root) (instance? java.io.File root))
+         (every? string? extensions)]}
+  (let [max-size (* max-size 1024 1024)]
+    (->> (apply f/find-by-extension root extensions)
+         f/paths->files
+         shuffle
+         (reductions (fn [s f] (conj s f)) #{})
+         (take-while (fn [s] (>= max-size (f/total-size s))))
+         last)))
